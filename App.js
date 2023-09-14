bf130eafdd3a0c4aef10fb21f8fa6257d2757497
An10113 <antranthanh1011@gmail.com>
2023-09-15 00:00:53 +0700

    bottom tab

--- App.js
+++ App.js
@@ -3,16 +3,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Landing from "./components/auth/Landing";
 import Register from "./components/auth/Register";
-import Main from "./components/Main";
+import MainScreen from "./components/Main";
 import Login from "./components/auth/Login";
 import { View } from "react-native-web";
 import { Text } from "react-native";
 
-import { createStore } from 'redux'
+import AddScreen from "./components/main/Add";
+import FeedScreen from "./components/main/Feed";
+import ProfileScreen from "./components/main/Profile";
+
+import { createStore } from "redux";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk'; // Replace with your chosen middleware
-import rootReducer from './redux/reducers'; // Import your root reducer
-const store = createStore(rootReducer, applyMiddleware(thunk))
+import thunk from "redux-thunk"; // Replace with your chosen middleware
+import rootReducer from "./redux/reducers"; // Import your root reducer
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -28,9 +32,9 @@ const firebaseConfig = {
   measurementId: "G-RT4ZV1CTPZ",
 };
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const db = getFirestore(app);
-export {app, db, auth}
+export { app, db, auth };
 
 const Stack = createStackNavigator();
 export class App extends Component {
@@ -86,7 +90,16 @@ export class App extends Component {
     if (loggedIn) {
       return (
         <Provider store={store}>
-        <Main></Main>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Main">
+              <Stack.Screen
+                name="Main"
+                component={MainScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="Add" component={AddScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </Provider>
       );
     }
--- components/Main.js
+++ components/Main.js
@@ -1,30 +1,68 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser } from "../redux/actions";
+import FeedScreen from "./main/Feed";
+import ProfileScreen from "./main/Profile";
+
+const Tab = createBottomTabNavigator();
+const Empty = () => {
+  return null;
+};
 
 export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
   render() {
-    const { currentUser } = this.props;
-    console.log(currentUser?.name);
-    if (!currentUser) {
-      return (
-        <View>
-          <Text>Logging in</Text>
-        </View>
-      );
-    } else {
-      return (
-        <View>
-          <Text> {currentUser?.name} is logged in</Text>
-        </View>
-      );
-    }
+    return (
+      <Tab.Navigator initialRouteName="Feed" screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="MainFeed"
+          component={FeedScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="MainAdd"
+          component={Empty}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+          listeners={({ navigation }) => ({
+            tabPress: (event) => {
+              event.preventDefault();
+              navigation.navigate("Add");
+            },
+          })}
+        />
+        <Tab.Screen
+          name="MainProfile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+            tabBarLabel: () => {
+              return null;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    );
   }
 }
 
